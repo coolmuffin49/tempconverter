1f7e9ea19a878088cf8eba29a930f28d3cca48dd
coolmuffin49 <coolmuffin49@gmail.com>
2019-05-14 16:39:28 +0700

    Updates

---
+++ precache-manifest.fc5ab4684cbde763d80564ec5d9ca527.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c4c37c6b23e0dadb4ddfeb74769954f",
+    "url": "/tempconverter/index.html"
+  },
+  {
+    "revision": "3fd1c7ed9944bcac3d4e",
+    "url": "/tempconverter/static/css/main.41d8ce20.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/tempconverter/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "3fd1c7ed9944bcac3d4e",
+    "url": "/tempconverter/static/js/main.18582350.chunk.js"
+  },
+  {
+    "revision": "13618a3a6ea2c02ca328",
+    "url": "/tempconverter/static/js/runtime~main.1dfb78bd.js"
+  },
+  {
+    "revision": "b23ff9688e9eb8b8681fbff5eee45938",
+    "url": "/tempconverter/static/media/sky.b23ff968.jpg"
+  },
+  {
+    "revision": "9646cdf13055763cd30842c62081dfd2",
+    "url": "/tempconverter/static/media/thermometer.9646cdf1.gif"
+  }
+]);
