4bc16668a3abc490d924941a14941747a35942af
coolmuffin49 <48484874+coolmuffin49@users.noreply.github.com>
2019-05-14 17:32:00 +0700

    Add files via upload

---
+++ src/Converter.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import Temp from './Temp';
+
+function toFah(celsius) {
+	return (celsius * 9 /5) + 32;
+}
+
+function toCel(fahrenheit) {
+	return (fahrenheit - 32) *5 /9;
+}
+
+function tryConvert(temp, funcName) {
+	const tempData = parseFloat(temp);
+	if (Number.isNaN(tempData)) {
+		return '';
+	}
+
+	const tempValue = funcName(tempData);
+	const finalTemp = Math.round(tempValue * 1000) / 1000;
+	return finalTemp.toString();
+}
+
+class Converter extends Component {
+	constructor(props) {
+		super(props);
+		this.onChangeC = this.onChangeC.bind(this);
+		this.onChangeF = this.onChangeF.bind(this);
+		this.state = {temp: '', unit: 'c'};
+	}
+
+	onChangeC(temp) {
+		this.setState({temp, unit: 'c'});
+	}	
+
+	onChangeF(temp) {
+		this.setState({temp, unit: 'f'});
+	}
+
+	render() {
+		const {unit, temp} = this.state;
+		
+		let celsius=unit;
+		if(unit==='f') {
+			celsius=tryConvert(temp, toCel)
+		} else {
+			celsius=temp;
+		}
+		let fahrenheit=unit;
+		if(unit==='c')	{
+			fahrenheit=tryConvert(temp, toFah)
+		} else {
+			fahrenheit=temp;
+		}
+	return (
+			<div>
+				<Temp
+					unit="c"
+					temp={celsius}
+					onTemp={this.onChangeC} 
+				/>
+
+				<Temp
+					unit="f"
+					temp={fahrenheit}
+					onTemp={this.onChangeF} 
+				/>
+			</div>
+		);
+	}
+}
+
+export default Converter;
